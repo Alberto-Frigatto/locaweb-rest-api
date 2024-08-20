63b3098feec59a6529063f918cad05f3996e527b
Gustavo Lopes <guhrlopes999@gmail.com>
2024-08-19 21:26:57 -0300

    feat: Criar classe FavoriteSentEmail no namespace Models

---
+++ Models/FavoriteSentEmail.cs
@@ -0,0 +1,11 @@
+﻿namespace locaweb_rest_api.Models
+{
+    public class FavoriteSentEmail
+    {
+        public int Id { get; set; }
+        public int IdUser { get; set; }
+        public User User { get; set; }
+        public int? IdReceivedEmail { get; set; }
+        public ReceivedEmail? ReceivedEmail { get; set; }
+   }
+}
