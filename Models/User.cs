c91e47919fd9a9d442f5a4beeb74d9fb9273d414
Gustavo Lopes <guhrlopes999@gmail.com>
2024-08-19 21:18:12 -0300

    feat: Criar classe User no namespace Models

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace locaweb_rest_api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public string Image {  get; set; }
+        public string Language { get; set; }
+        public bool Theme { get; set; }
+
+    }
+}
