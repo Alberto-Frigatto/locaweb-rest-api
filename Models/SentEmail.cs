acefa021accddd7a20e0137452dfd3b1ceda5737
Gustavo Lopes <guhrlopes999@gmail.com>
2024-08-19 21:18:28 -0300

    Criar classe SentEmail no namespace Models

---
+++ Models/SentEmail.cs
@@ -0,0 +1,15 @@
+﻿namespace locaweb_rest_api.Models
+{
+    public class SentEmail
+    {
+        public int Id { get; set; }
+        public string Recipient { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public DateTime SendDate { get; set; }
+        public bool Viewed { get; set; }
+        public bool Scheduled { get; set; }
+        public User User { get; set; }
+    }
+}
