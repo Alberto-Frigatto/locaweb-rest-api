8249e9af2a2161c38d4dad2faec07dada3ae25bc
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-08-19 21:55:19 -0300

    chore: Mapear classe DatabaseContext na classe Program

--- Program.cs
+++ Program.cs
@@ -1,15 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using locaweb_rest_api.Data.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DatabaseConnection");
+builder.Services.AddDbContext<DatabaseContext>(
+    opt => opt.UseOracle(connectionString).EnableSensitiveDataLogging(true)
+);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
