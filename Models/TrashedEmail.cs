ba1413b1bb6a3148070eea20e16dae14037540ea
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-08-19 21:23:50 -0300

    feat: Criar classe TrashedEmail no namespace Models

---
+++ Models/TrashedEmail.cs
@@ -0,0 +1,13 @@
+﻿namespace locaweb_rest_api.Models
+{
+    public class TrashedEmail
+    {
+        public int Id { get; set; }
+        public int? IdReceivedEmail { get; set; }
+        public ReceivedEmail? ReceivedEmail { get; set; }
+        public int? IdSentEmail { get; set; }
+        public SentEmail? SentEmail { get; set; }
+        public int IdUser { get; set; }
+        public User User { get; set; }
+    }
+}
