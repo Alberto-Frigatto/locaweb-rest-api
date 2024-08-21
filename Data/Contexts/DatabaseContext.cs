c99c05a83f57cb8e2dc7cb001b7d6c03faaf7e6d
Gustavo Lopes <guhrlopes999@gmail.com>
2024-08-20 21:01:23 -0300

    feat: Mapear as classes Model na classe DatabaseContext

--- Data/Contexts/DatabaseContext.cs
+++ Data/Contexts/DatabaseContext.cs
@@ -27,6 +27,47 @@ namespace locaweb_rest_api.Data.Contexts
                 entity.Property(p => p.Language).IsRequired().HasMaxLength(2);
                 entity.Property(p => p.Theme).IsRequired();
             });
+
+            modelBuilder.Entity<SentEmail>(entity => { 
+                entity.ToTable("SentEmail");
+                entity.HasKey(p => p.Id);
+                entity.Property(p => p.Recipient).IsRequired().HasMaxLength(150);
+                entity.Property(p => p.Subject).IsRequired().HasMaxLength(255);
+                entity.Property(p => p.Body).IsRequired().HasMaxLength(255);
+                entity.Property(p => p.TimeStamp).IsRequired().HasColumnType("date");
+                entity.Property(p => p.SendDate).IsRequired().HasColumnType("date");
+                entity.Property(p => p.Viewed).IsRequired().HasColumnType("number(1)");
+                entity.Property(p => p.Scheduled).IsRequired().HasColumnType("number(1)");
+                entity.HasOne(e => e.User).WithMany().HasForeignKey(e => e.IdUser).IsRequired();
+            });
+
+            modelBuilder.Entity<ReceivedEmail>(entity =>
+            {
+                entity.ToTable("ReceivedEmail");
+                entity.HasKey(p => p.Id);
+                entity.Property(p => p.Sender).IsRequired().HasMaxLength(150);
+                entity.Property(p => p.Recipient).IsRequired().HasMaxLength(150);
+                entity.Property(p => p.Subject).IsRequired().HasMaxLength(255);
+                entity.Property(p => p.Body).IsRequired().HasMaxLength(255);
+                entity.Property(p => p.Timestamp).IsRequired().HasColumnType("date");
+            });
+
+            modelBuilder.Entity<TrashedEmail>(entity =>
+            {
+                entity.ToTable("TrashedEmail");
+                entity.HasKey(p => p.Id);
+                entity.HasOne(e => e.ReceivedEmail).WithMany().HasForeignKey(e => e.IdReceivedEmail).IsRequired();
+                entity.HasOne(e => e.SentEmail).WithMany().HasForeignKey(e => e.IdSentEmail).IsRequired();
+                entity.HasOne(e => e.User).WithMany().HasForeignKey(e => e.IdUser).IsRequired();
+            });
+
+            modelBuilder.Entity<FavoriteSentEmail>(entity =>
+            {
+                entity.ToTable("FavoriteSentEmail");
+                entity.HasKey(p => p.Id);
+                entity.HasOne(e => e.User).WithMany().HasForeignKey(e => e.IdUser).IsRequired();
+                entity.HasOne(e => e.ReceivedEmail).WithMany().HasForeignKey(e => e.IdReceivedEmail).IsRequired();
+            });
         }
     }
 }
