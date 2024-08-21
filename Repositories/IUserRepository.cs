ff9e623874c5b568506514147877bed24f00113a
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-08-20 22:06:08 -0300

    feat: Criar interface IUserRepository no namespace Repositories

---
+++ Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using locaweb_rest_api.Models;
+
+namespace locaweb_rest_api.Repositories
+{
+    public interface IUserRepository
+    {
+        User? GetByEmailAndPassword(string email, string password);
+        User? GetByEmail(string email);
+        void Add(User model);
+        void Update(User model);
+    }
+}
