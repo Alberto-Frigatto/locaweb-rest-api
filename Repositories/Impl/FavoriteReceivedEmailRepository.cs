3087dde4d271bc3f6d133275832591a72bf42e1b
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-08-23 22:53:55 -0300

    feat: Criar classe FavoriteReceivedEmailRepository no namespace Repositories.Impl

---
+++ Repositories/Impl/FavoriteReceivedEmailRepository.cs
@@ -0,0 +1,39 @@
+﻿using locaweb_rest_api.Models;
+using locaweb_rest_api.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace locaweb_rest_api.Repositories.Impl
+{
+    public class FavoriteReceivedEmailRepository : IFavoriteReceivedEmailRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public FavoriteReceivedEmailRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(FavoriteReceivedEmail model)
+        {
+            _context.FavoriteReceivedEmails.Add(model);
+            _context.SaveChanges();
+        }
+
+        public void Delete(FavoriteReceivedEmail model)
+        {
+            _context.FavoriteReceivedEmails.Remove(model);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<FavoriteReceivedEmail> GetAll(int page)
+        {
+            return _context.FavoriteReceivedEmails
+                .Include(e => e.User)
+                .Include(e => e.ReceivedEmail)
+                .Skip((page - 1) * page)
+                .Take(20)
+                .AsNoTracking()
+                .ToList();
+        }
+    }
+}
