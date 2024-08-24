641dca5839f8c9d323a78c01ff0153d45416d03a
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-08-23 22:48:43 -0300

    feat: Criar interface IFavoriteReceivedEmailRepository no namespace Repositories

---
+++ Repositories/IFavoriteReceivedEmailRepository.cs
@@ -0,0 +1,11 @@
+﻿using locaweb_rest_api.Models;
+
+namespace locaweb_rest_api.Repositories
+{
+    public interface IFavoriteReceivedEmailRepository
+    {
+        IEnumerable<FavoriteReceivedEmail> GetAll(int page);
+        void Add(FavoriteReceivedEmail model);
+        void Delete(FavoriteReceivedEmail model);
+    }
+}
