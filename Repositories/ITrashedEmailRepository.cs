39680ba1bb2c7c5a088973cf93da697d890d0de8
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-08-23 22:47:29 -0300

    feat: Criar interface ITrashedEmailRepository no namespace Repositories

---
+++ Repositories/ITrashedEmailRepository.cs
@@ -0,0 +1,11 @@
+﻿using locaweb_rest_api.Models;
+
+namespace locaweb_rest_api.Repositories
+{
+    public interface ITrashedEmailRepository
+    {
+        IEnumerable<TrashedEmail> GetAll(int page);
+        void Add(TrashedEmail model);
+        void Delete(TrashedEmail model);
+    }
+}
