daf6ea5fae22d2c9026cd3956c90c0100595cc06
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-08-23 22:53:25 -0300

    feat: Criar classe DeletedReceivedEmailRepository no namespace Repositories.Impl

---
+++ Repositories/Impl/DeletedReceivedEmailRepository.cs
@@ -0,0 +1,21 @@
+﻿using locaweb_rest_api.Models;
+using locaweb_rest_api.Data.Contexts;
+
+namespace locaweb_rest_api.Repositories.Impl
+{
+    public class DeletedReceivedEmailRepository : IDeletedReceivedEmailRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public DeletedReceivedEmailRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(DeletedReceivedEmail model)
+        {
+            _context.DeletedEmails.Add(model);
+            _context.SaveChanges();
+        }
+    }
+}
