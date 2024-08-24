306481895453cb04edc75ea2002533197d4db722
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-08-23 22:54:22 -0300

    feat: Criar classe TrashedEmailRepository no namespace Repositories.Impl

---
+++ Repositories/Impl/TrashedEmailRepository.cs
@@ -0,0 +1,40 @@
+﻿using locaweb_rest_api.Models;
+using locaweb_rest_api.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace locaweb_rest_api.Repositories.Impl
+{
+    public class TrashedEmailRepository : ITrashedEmailRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public TrashedEmailRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(TrashedEmail model)
+        {
+            _context.TrashedEmails.Add(model);
+            _context.SaveChanges();
+        }
+
+        public void Delete(TrashedEmail model)
+        {
+            _context.TrashedEmails.Remove(model);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<TrashedEmail> GetAll(int page)
+        {
+            return _context.TrashedEmails
+                .Include(e => e.User)
+                .Include(e => e.ReceivedEmail)
+                .Include(e => e.SentEmail)
+                .Skip((page - 1) * page)
+                .Take(20)
+                .AsNoTracking()
+                .ToList();
+        }
+    }
+}
