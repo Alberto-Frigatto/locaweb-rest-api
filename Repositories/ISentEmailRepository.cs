89031c08aa134e8a3085f8c3445744fba221c9c7
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-08-23 22:47:18 -0300

    feat: Criar interface ISentEmailRepository no namespace Repositories

---
+++ Repositories/ISentEmailRepository.cs
@@ -0,0 +1,13 @@
+﻿using locaweb_rest_api.Models;
+
+namespace locaweb_rest_api.Repositories
+{
+    public interface ISentEmailRepository
+    {
+        IEnumerable<SentEmail> GetAll(int page);
+        SentEmail? GetById(int id);
+        void Add(SentEmail model);
+        void Update(SentEmail model);
+        void Delete(SentEmail model);
+    }
+}
