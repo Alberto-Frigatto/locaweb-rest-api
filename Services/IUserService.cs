c5b6a263eb1319f815c6e7da2099778940055ff4
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-08-28 15:37:42 -0300

    feat: Criar interface IUserService no namespace Services

---
+++ Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using locaweb_rest_api.Models;
+
+namespace locaweb_rest_api.Services
+{
+    public interface IUserService
+    {
+        User? GetUserByEmail(string email);
+        void CreateUser(User model, IFormFile image);
+    }
+}
