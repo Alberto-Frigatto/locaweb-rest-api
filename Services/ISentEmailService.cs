c69430d376742ddfd8de700d67da35724d66c7f4
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-08-28 15:38:23 -0300

    feat: Criar interface ISentEmailService no namespace Services

---
+++ Services/ISentEmailService.cs
@@ -0,0 +1,13 @@
+﻿using locaweb_rest_api.Models;
+
+namespace locaweb_rest_api.Services
+{
+    public interface ISentEmailService
+    {
+        IEnumerable<SentEmail> GetAllSentEmails(int page);
+        SentEmail? GetSentEmailById(int id);
+        void CreateSentEmail(SentEmail model);
+        void UpdateSentEmail(SentEmail model);
+        void DeleteSentEmail(int id);
+    }
+}
