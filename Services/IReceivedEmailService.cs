4a8aef86d5be5c94e2edd4360ac0556c7198cd55
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-08-28 15:38:54 -0300

    feat: Criar interface IReceivedEmailService no namespace Services

---
+++ Services/IReceivedEmailService.cs
@@ -0,0 +1,9 @@
+﻿using locaweb_rest_api.Models;
+
+namespace locaweb_rest_api.Services
+{
+    public interface IReceivedEmailService
+    {
+        IEnumerable<ReceivedEmail> GetAllReceivedEmails(int page);
+    }
+}
