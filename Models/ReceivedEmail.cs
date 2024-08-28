c1ad60c5b08d19047edb35f7ee5edb105c75dffc
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-08-28 16:46:17 -0300

    refactor: Remover atributo Recipient da classe ReceivedEmail

--- Models/ReceivedEmail.cs
+++ Models/ReceivedEmail.cs
@@ -7,7 +7,6 @@ namespace locaweb_rest_api.Models
     {
         public int Id { get; set; }
         public string Sender { get; set; }
-        public string Recipient { get; set; }
         public string Subject { get; set; }
         public string Body { get; set; }
         public DateTime Timestamp { get; set; }
