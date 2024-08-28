1b2fc70e2a820a5ae35150bebcc711606c99572f
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-08-28 15:41:53 -0300

    feat: Criar classe SentEmailService no namespace Services.Impl

---
+++ Services/Impl/SentEmailService.cs
@@ -0,0 +1,43 @@
+﻿using locaweb_rest_api.Models;
+using locaweb_rest_api.Repositories;
+
+namespace locaweb_rest_api.Services.Impl
+{
+    public class SentEmailService : ISentEmailService
+    {
+        private readonly ISentEmailRepository _repository;
+
+        public SentEmailService(ISentEmailRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void CreateSentEmail(SentEmail model)
+        {
+            _repository.Add(model);
+        }
+
+        public void DeleteSentEmail(int id)
+        {
+            SentEmail? sentEmail = _repository.GetById(id);
+
+            if (sentEmail != null)
+                _repository.Delete(sentEmail);
+        }
+
+        public IEnumerable<SentEmail> GetAllSentEmails(int page)
+        {
+            return _repository.GetAll(page);
+        }
+
+        public SentEmail? GetSentEmailById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public void UpdateSentEmail(SentEmail model)
+        {
+            _repository.Update(model);
+        }
+    }
+}
