1cd65afe4a39d9c1d5f2467adcc2c4a3597a3ea1
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-08-28 15:38:34 -0300

    feat: Criar interface IDeletedReceivedEmailService no namespace Services

---
+++ Services/IDeletedReceivedEmailService.cs
@@ -0,0 +1,9 @@
+﻿using locaweb_rest_api.Models;
+
+namespace locaweb_rest_api.Services
+{
+    public interface IDeletedReceivedEmailService
+    {
+        void CreateDeletedReceivedEmail(DeletedReceivedEmail model);
+    }
+}
