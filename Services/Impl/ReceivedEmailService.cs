f80a5c5c573c338b8fed6738280396a4f440cfb7
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-08-28 15:41:43 -0300

    feat: Criar classe ReceivedEmailService no namespace Services.Impl

---
+++ Services/Impl/ReceivedEmailService.cs
@@ -0,0 +1,20 @@
+﻿using locaweb_rest_api.Models;
+using locaweb_rest_api.Repositories;
+
+namespace locaweb_rest_api.Services.Impl
+{
+    public class ReceivedEmailService : IReceivedEmailService
+    {
+        private readonly IReceivedEmailRepository _repository;
+
+        public ReceivedEmailService(IReceivedEmailRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<ReceivedEmail> GetAllReceivedEmails(int page)
+        {
+            return _repository.GetAll(page)
+        }
+    }
+}
