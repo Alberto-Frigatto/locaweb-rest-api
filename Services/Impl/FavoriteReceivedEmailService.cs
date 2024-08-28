94b65455d079f184e4ff80a07baf289ad5d3ea29
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-08-28 15:40:51 -0300

    feat: Criar classe FavoriteReceivedEmailService no namespace Services.Impl

---
+++ Services/Impl/FavoriteReceivedEmailService.cs
@@ -0,0 +1,33 @@
+﻿using locaweb_rest_api.Models;
+using locaweb_rest_api.Repositories;
+
+namespace locaweb_rest_api.Services.Impl
+{
+    public class FavoriteReceivedEmailService : IFavoriteReceivedEmailService
+    {
+        private readonly IFavoriteReceivedEmailRepository _repository;
+
+        public FavoriteReceivedEmailService(IFavoriteReceivedEmailRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void CreateFavoriteReceivedEmail(FavoriteReceivedEmail model)
+        {
+            _repository.Add(model);
+        }
+
+        public void DeleteFavoriteReceivedEmail(int idReceivedEmail)
+        {
+            FavoriteReceivedEmail? favoriteReceivedEmail = _repository.GetByIdReceivedEmail(idReceivedEmail);
+
+            if (favoriteReceivedEmail != null)
+                _repository.Delete(favoriteReceivedEmail);
+        }
+
+        public IEnumerable<FavoriteReceivedEmail> GetAllFavoriteReceivedEmails(int page)
+        {
+            return _repository.GetAll(page);
+        }
+    }
+}
