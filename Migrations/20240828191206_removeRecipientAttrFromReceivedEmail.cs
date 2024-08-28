f958791cb63a4bc07e129aa28618e055fc4b7fa0
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-08-28 16:47:13 -0300

    chore: Adicionar migração para remover atributo da classe ReceivedEmail

---
+++ Migrations/20240828191206_removeRecipientAttrFromReceivedEmail.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace locaweb_rest_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeRecipientAttrFromReceivedEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Recipient",
+                table: "ReceivedEmail");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Recipient",
+                table: "ReceivedEmail",
+                type: "NVARCHAR2(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
