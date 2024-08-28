859a5bf52dc2ef029a1f28e235b52a26e05d66e7
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-08-28 15:38:10 -0300

    feat: Criar interface ITrashedEmailService no namespace Services

---
+++ Services/ITrashedEmailService.cs
@@ -0,0 +1,12 @@
+﻿using locaweb_rest_api.Models;
+
+namespace locaweb_rest_api.Services
+{
+    public interface ITrashedEmailService
+    {
+        IEnumerable<TrashedEmail> GetAllTrashedEmails(int page);
+        void CreateTrashedEmail(TrashedEmail model);
+        void DeleteTrashedEmailByIdReceivedEmail(int idReceivedEmail);
+        void DeleteTrashedEmailByIdSentEmail(int idSentEmail);
+    }
+}
