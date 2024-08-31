0a7eb0b75b7d028f9bb1808fc4c8adbe3e961155
Gustavo Lopes <guhrlopes999@gmail.com>
2024-08-31 16:19:35 -0300

    chore: Adicionar autenticação

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ using locaweb_rest_api.Services;
 using locaweb_rest_api.Services.Impl;
 using locaweb_rest_api.Repositories;
 using locaweb_rest_api.Repositories.Impl;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +29,22 @@ builder.Services.AddScoped<IReceivedEmailService, ReceivedEmailService>();
 builder.Services.AddScoped<ISentEmailService, SentEmailService>();
 builder.Services.AddScoped<ITrashedEmailService, TrashedEmailService>();
 builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("f+ujXAKHk00L5jlMXo2XhAWawsOoihNP1OiAM25lLSO57+X7uBMQgwPju6yzyePi")),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
