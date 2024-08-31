16aef4cacc1660b7a51a069c523543a2a90b9449
Gustavo Lopes <guhrlopes999@gmail.com>
2024-08-31 16:24:20 -0300

    feat: Criar OutReceivedEmailViewModel no namespace ViewModel.Out

---
+++ ViewModels/Out/OutReceivedEmailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace locaweb_rest_api.ViewModels.Out
+{
+    public class OutReceivedEmailViewModel
+    {
+        public int Id { get; set; }
+        public string Sender { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
