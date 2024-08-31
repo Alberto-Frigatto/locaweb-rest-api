8c4a8f32c143227c8a7a46f2bd1977cfa441aa20
Gustavo Lopes <guhrlopes999@gmail.com>
2024-08-31 16:23:54 -0300

    feat: Criar OutSentEmailViewModel no namespace ViewModel.Out

---
+++ ViewModels/Out/OutSentEmailViewModel.cs
@@ -0,0 +1,18 @@
+﻿using locaweb_rest_api.Models;
+
+namespace locaweb_rest_api.ViewModels.Out
+{
+    public class OutSentEmailViewModel
+    {
+        public int Id { get; set; }
+        public string Recipient { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public DateTime SendDate { get; set; }
+        public bool Viewed { get; set; }
+        public bool Scheduled { get; set; }
+        public bool Canceled { get; set; }
+        public User User { get; set; }
+    }
+}
