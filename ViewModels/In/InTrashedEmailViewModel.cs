a8e45a8ac57ab9d5afff29c1a13543b24552fb53
Gustavo Lopes <guhrlopes999@gmail.com>
2024-08-31 16:21:41 -0300

    feat: Criar InTrashedViewModel no namespace ViewModel.In

---
+++ ViewModels/In/InTrashedEmailViewModel.cs
@@ -0,0 +1,20 @@
+﻿using locaweb_rest_api.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace locaweb_rest_api.ViewModels.In
+{
+    public class InTrashedEmailViewModel
+    {
+        [Required(ErrorMessage = "O id do e-mail recebido é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "O id do e-mail recebido é inválido")]
+        public int? IdReceivedEmail { get; set; }
+
+        [Required(ErrorMessage = "O id do e-mail enviado é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "O id do e-mail enviado é inválido")]
+        public int? IdSentEmail { get; set; }
+
+        [Required(ErrorMessage = "O id do usuário é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "O id do usuário é inválido")]
+        public int IdUser { get; set; }
+    }
+}
