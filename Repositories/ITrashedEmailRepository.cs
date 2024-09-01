f7e38e6a8af7edcf576559ef516bcbbf3a1b1211
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-09-01 20:58:11 -0300

    refactor: Alterar métodos da interface ITrashedEmailRepository
    - Adiciona o parâmetro idUser no método GetAll
    - Renomeia o método GetByIdReceivedEmail para GetByIdUserAndIdReceivedEmail
    - Adiciona o parâmetro idUser no método GetByIdUserAndIdReceivedEmail
    - Renomeia o método GetByIdSentEmail para GetByIdUserAndIdSentEmail
    - Adiciona o parâmetro idUser no método GetByIdUserAndIdSentEmail

--- Repositories/ITrashedEmailRepository.cs
+++ Repositories/ITrashedEmailRepository.cs
@@ -4,10 +4,10 @@ namespace locaweb_rest_api.Repositories
 {
     public interface ITrashedEmailRepository
     {
-        IEnumerable<TrashedEmail> GetAll(int page);
+        IEnumerable<TrashedEmail> GetAll(int page, int idUser);
         void Add(TrashedEmail model);
         void Delete(TrashedEmail model);
-        TrashedEmail? GetByIdReceivedEmail(int idReceivedEmail);
-        TrashedEmail? GetByIdSentEmail(int idSentEmail);
+        TrashedEmail? GetByIdUserAndIdReceivedEmail(int idUser, int idReceivedEmail);
+        TrashedEmail? GetByIdUserAndIdSentEmail(int idUser, int idSentEmail);
     }
 }
