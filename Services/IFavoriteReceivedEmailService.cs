d51b365c3d6f494bfbbd66e94104263774052faf
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-09-01 21:18:20 -0300

    refactor: Alterar métodos na interface IFavoriteReceivedEmailService
    - Adiciona o parâmetro idUser aos métodos GetAllFavoriteReceivedEmails e DeleteFavoriteReceivedEmail

--- Services/IFavoriteReceivedEmailService.cs
+++ Services/IFavoriteReceivedEmailService.cs
@@ -4,8 +4,8 @@ namespace locaweb_rest_api.Services
 {
     public interface IFavoriteReceivedEmailService
     {
-        IEnumerable<FavoriteReceivedEmail> GetAllFavoriteReceivedEmails(int page);
+        IEnumerable<FavoriteReceivedEmail> GetAllFavoriteReceivedEmails(int page, int idUser);
         void CreateFavoriteReceivedEmail(FavoriteReceivedEmail model);
-        void DeleteFavoriteReceivedEmail(int idReceivedEmail);
+        void DeleteFavoriteReceivedEmail(int idReceivedEmail, int idUser);
     }
 }
