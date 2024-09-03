f1aa2ab87ee93773a14764276604484d0cbbf84c
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-09-02 21:14:30 -0300

    feat: Criar a interface IAuthService no namespace Services

---
+++ Services/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using locaweb_rest_api.Models;
+
+namespace locaweb_rest_api.Services
+{
+    public interface IAuthService
+    {
+        User? Authenticate(string email, string password);
+        string GenerateJwtToken(User user);
+    }
+}
