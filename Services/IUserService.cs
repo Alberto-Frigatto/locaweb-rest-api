b7cbd9f632cdfb499216191528c8a481948ab7fd
Gustavo Lopes <guhrlopes999@gmail.com>
2024-09-03 16:36:24 -0300

    feat: Adicionar método GetUserById e UpdateUserPreferences na interface IUserService

--- Services/IUserService.cs
+++ Services/IUserService.cs
@@ -5,6 +5,8 @@ namespace locaweb_rest_api.Services
     public interface IUserService
     {
         User? GetUserByEmail(string email);
+        User? GetUserById(int id);
         void CreateUser(User model, IFormFile image);
+        void UpdateUserPreferences(User model);
     }
 }
