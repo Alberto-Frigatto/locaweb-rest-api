00119ff3c1def2caa06664dcc7f940b0c27ece4b
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-09-02 21:21:49 -0300

    refactor: Alterar classe ReceivedEmailService
    - Adicionar dependência dos repositórios ITrashedEmailRepository, IFavoriteReceivedEmailRepository e IDeletedReceivedEmailRepository
    - Adaptar a assinatura do método GetAllReceivedEmails para satisfazer a interface
    - Adapta o método GetAllReceivedEmails para alterar a lista de itens retornada pelo repositório para inserir informações nos models

--- Services/Impl/ReceivedEmailService.cs
+++ Services/Impl/ReceivedEmailService.cs
@@ -5,16 +5,36 @@ namespace locaweb_rest_api.Services.Impl
 {
     public class ReceivedEmailService : IReceivedEmailService
     {
-        private readonly IReceivedEmailRepository _repository;
+        private readonly IReceivedEmailRepository _receivedEmailRepository;
+        private readonly ITrashedEmailRepository _trashedEmailRepository;
+        private readonly IFavoriteReceivedEmailRepository _favoriteReceivedEmailRepository;
+        private readonly IDeletedReceivedEmailRepository _deletedReceivedEmailRepository;
 
-        public ReceivedEmailService(IReceivedEmailRepository repository)
-        {
-            _repository = repository;
+        public ReceivedEmailService(
+            IReceivedEmailRepository receivedEmailRepository,
+            ITrashedEmailRepository trashedEmailRepository,
+            IFavoriteReceivedEmailRepository favoriteReceivedEmailRepository,
+            IDeletedReceivedEmailRepository deletedReceivedEmailRepository
+        ) {
+            _receivedEmailRepository = receivedEmailRepository;
+            _trashedEmailRepository = trashedEmailRepository;
+            _favoriteReceivedEmailRepository = favoriteReceivedEmailRepository;
+            _deletedReceivedEmailRepository = deletedReceivedEmailRepository;
         }
 
-        public IEnumerable<ReceivedEmail> GetAllReceivedEmails(int page)
+        public IEnumerable<ReceivedEmail> GetAllReceivedEmails(int page, int idUser)
         {
-            return _repository.GetAll(page);
+            List<ReceivedEmail> allReceivedEmails = _receivedEmailRepository.GetAll(page).ToList();
+            allReceivedEmails.ForEach(receivedEmail => {
+                receivedEmail.IsDeleted = _deletedReceivedEmailRepository
+                    .GetByIdUserAndIdReceivedEmail(idUser, receivedEmail.Id) != null;
+                receivedEmail.IsTrash = _trashedEmailRepository
+                    .GetByIdUserAndIdReceivedEmail(idUser, receivedEmail.Id) != null;
+                receivedEmail.IsFavorite = _favoriteReceivedEmailRepository
+                    .GetByIdUserAndIdReceivedEmail(idUser, receivedEmail.Id) != null;
+            });
+
+            return allReceivedEmails;
         }
     }
 }
