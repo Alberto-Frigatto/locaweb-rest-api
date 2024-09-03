503a908b728136bd32cbb92e8fbe35d48c202629
Gustavo Lopes <guhrlopes999@gmail.com>
2024-09-03 17:10:03 -0300

    feat: Criar classe InUpdateUserPreferencesViewModel no namespace ViewModels.In

---
+++ ViewModels/In/InUpdateUserPreferencesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace locaweb_rest_api.ViewModels.In
+{
+    public class InUpdateUserPreferencesViewModel
+    {
+        [Required(ErrorMessage = "O idioma é obrigatório")]
+        [MinLength(2, ErrorMessage = "O idioma deve ter 2 caracteres")]
+        [MaxLength(2, ErrorMessage = "O idioma deve ter 2 caracteres")]
+        [RegularExpression("^(pt|en)$", ErrorMessage = "O idioma deve ser 'pt' ou 'en'")]
+        public required string Language { get; set; }
+
+        [Required(ErrorMessage = "O tema é obrigatório")]
+        public bool Theme { get; set; }
+    }
+}
