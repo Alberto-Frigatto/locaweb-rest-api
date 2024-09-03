79af1e064ebdef42e93313cbbf2118ad61b76a8b
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-09-02 21:07:09 -0300

    chore: Adicionar migração para remover não nulidade de atributos de TrashedEmail

--- Migrations/20240902222830_UpdateTrashedEmailWithNUllableAttrs.Designer.cs
+++ Migrations/20240902222830_UpdateTrashedEmailWithNUllableAttrs.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Oracle.EntityFrameworkCore.Metadata;
 using locaweb_rest_api.Data.Contexts;
@@ -11,9 +12,11 @@ using locaweb_rest_api.Data.Contexts;
 namespace locaweb_rest_api.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240902222830_UpdateTrashedEmailWithNUllableAttrs")]
+    partial class UpdateTrashedEmailWithNUllableAttrs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240902222830_UpdateTrashedEmailWithNUllableAttrs.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace locaweb_rest_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTrashedEmailWithNUllableAttrs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TrashedEmail_ReceivedEmail_IdReceivedEmail",
+                table: "TrashedEmail");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TrashedEmail_SentEmail_IdSentEmail",
+                table: "TrashedEmail");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IdSentEmail",
+                table: "TrashedEmail",
+                type: "NUMBER(10)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "NUMBER(10)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IdReceivedEmail",
+                table: "TrashedEmail",
+                type: "NUMBER(10)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "NUMBER(10)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TrashedEmail_ReceivedEmail_IdReceivedEmail",
+                table: "TrashedEmail",
+                column: "IdReceivedEmail",
+                principalTable: "ReceivedEmail",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TrashedEmail_SentEmail_IdSentEmail",
+                table: "TrashedEmail",
+                column: "IdSentEmail",
+                principalTable: "SentEmail",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TrashedEmail_ReceivedEmail_IdReceivedEmail",
+                table: "TrashedEmail");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TrashedEmail_SentEmail_IdSentEmail",
+                table: "TrashedEmail");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IdSentEmail",
+                table: "TrashedEmail",
+                type: "NUMBER(10)",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "NUMBER(10)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IdReceivedEmail",
+                table: "TrashedEmail",
+                type: "NUMBER(10)",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "NUMBER(10)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TrashedEmail_ReceivedEmail_IdReceivedEmail",
+                table: "TrashedEmail",
+                column: "IdReceivedEmail",
+                principalTable: "ReceivedEmail",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TrashedEmail_SentEmail_IdSentEmail",
+                table: "TrashedEmail",
+                column: "IdSentEmail",
+                principalTable: "SentEmail",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
