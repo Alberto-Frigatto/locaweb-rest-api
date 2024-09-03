f45654b2c93f1fc3b49983128425d1f7819c0a13
Gustavo Lopes <guhrlopes999@gmail.com>
2024-09-03 17:09:42 -0300

    refactor: Adicionar modificador required nos atributos da classe InLoginViewModel

--- ViewModels/In/InLoginViewModel.cs
+++ ViewModels/In/InLoginViewModel.cs
@@ -7,11 +7,11 @@ namespace locaweb_rest_api.ViewModels.In
         [Required(ErrorMessage = "O e-mail é obrigatório")]
         [MinLength(1, ErrorMessage = "O e-mail não pode ser vazio")]
         [MaxLength(255, ErrorMessage = "O e-mail não deve exceder 150 caracteres")]
-        public string Email { get; set; }
+        public required string Email { get; set; }
 
         [Required(ErrorMessage = "A senha é obrigatória")]
         [MinLength(1, ErrorMessage = "A senha não pode ser vazia")]
         [MaxLength(255, ErrorMessage = "A senha não pode exceder 255 caracteres")]
-        public string Password { get; set; }
+        public required string Password { get; set; }
     }
 }
