d233e942f673a8d5256e81c3c4aca940b937ca6b
Gustavo Lopes <guhrlopes999@gmail.com>
2024-09-03 17:08:45 -0300

    refactor: Adicionar atributos IsFavorite e Imaga na classe OutReceivedEmailViewModel

--- ViewModels/Out/OutReceivedEmailViewModel.cs
+++ ViewModels/Out/OutReceivedEmailViewModel.cs
@@ -9,5 +9,7 @@ namespace locaweb_rest_api.ViewModels.Out
         public string Subject { get; set; }
         public string Body { get; set; }
         public DateTime Timestamp { get; set; }
+        public bool IsFavorite { get; set; }
+        public string Image { get; set; }
     }
 }
