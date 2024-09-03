1582f9069406cf6c4001f30f3697df7d86d5394d
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-09-02 21:15:13 -0300

    feat: Criar classe AuthService no namespace Services.Impl

---
+++ Services/Impl/AuthService.cs
@@ -0,0 +1,49 @@
+﻿using locaweb_rest_api.Models;
+using locaweb_rest_api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace locaweb_rest_api.Services.Impl
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IUserRepository _repository;
+
+        public AuthService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public User? Authenticate(string email, string password)
+        {
+            return _repository.GetByEmailAndPassword(email, password);
+        }
+
+        public string GenerateJwtToken(User user)
+        {
+            byte[] secret = Encoding.ASCII.GetBytes("f+ujXAKHk00L5jlMXo2XhAWawsOoihNP1OiAM25lLSO57+X7uBMQgwPju6yzyePi");
+            var securityKey = new SymmetricSecurityKey(secret);
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            JwtSecurityTokenHandler jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            SecurityTokenDescriptor securityTokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Hash, Guid.NewGuid().ToString())
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(20),
+                Issuer = "fiap",
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(secret),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+            SecurityToken securityToken = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+
+            return new JwtSecurityTokenHandler().WriteToken(securityToken);
+        }
+    }
+}
