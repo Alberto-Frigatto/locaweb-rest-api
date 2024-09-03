bc9cf62acb52e7cf78742f96bbbf5599f60902ae
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-09-02 21:28:08 -0300

    feat: Criar classe OutSentEmailViewModel no namespace ViewModels.In

---
+++ ViewModels/In/InLoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace locaweb_rest_api.ViewModels.In
+{
+    public class InLoginViewModel
+    {
+        [Required(ErrorMessage = "O e-mail é obrigatório")]
+        [MinLength(1, ErrorMessage = "O e-mail não pode ser vazio")]
+        [MaxLength(255, ErrorMessage = "O e-mail não deve exceder 150 caracteres")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatória")]
+        [MinLength(1, ErrorMessage = "A senha não pode ser vazia")]
+        [MaxLength(255, ErrorMessage = "A senha não pode exceder 255 caracteres")]
+        public string Password { get; set; }
+    }
+}
