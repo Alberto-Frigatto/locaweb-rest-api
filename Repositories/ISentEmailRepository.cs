c934975e4718eb8769e13e3f5d760bd98a965d43
Gustavo Lopes <guhrlopes999@gmail.com>
2024-09-04 14:50:16 -0300

    fix: Alterar método GetAll na interface ISentEmailRepository
    Adiciona o parâmetro idUser

--- Repositories/ISentEmailRepository.cs
+++ Repositories/ISentEmailRepository.cs
@@ -4,7 +4,7 @@ namespace locaweb_rest_api.Repositories
 {
     public interface ISentEmailRepository
     {
-        IEnumerable<SentEmail> GetAll(int page);
+        IEnumerable<SentEmail> GetAll(int page, int idUser);
         SentEmail? GetById(int id);
         void Add(SentEmail model);
         void Update(SentEmail model);
