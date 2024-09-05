1a4632b30dd434575b6efaa66928a916edcd9fc7
Gustavo Lopes <guhrlopes999@gmail.com>
2024-09-05 15:37:32 -0300

    feat: Adicionar método GetReceivedEmailById na interface IReceivedEmailService

--- Services/IReceivedEmailService.cs
+++ Services/IReceivedEmailService.cs
@@ -5,5 +5,6 @@ namespace locaweb_rest_api.Services
     public interface IReceivedEmailService
     {
         IEnumerable<ReceivedEmail> GetAllReceivedEmails(int page, int idUser);
+        ReceivedEmail? GetReceivedEmailById(int id, int idUser);
     }
 }
