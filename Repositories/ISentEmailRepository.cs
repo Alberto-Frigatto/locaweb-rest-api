6411c4c73319e037e50d3fe68b412090fa21099f
Gustavo Lopes <guhrlopes999@gmail.com>
2024-09-05 13:25:51 -0300

    feat: Adicionar método GetLast na interface ISentEmailRepository

--- Repositories/ISentEmailRepository.cs
+++ Repositories/ISentEmailRepository.cs
@@ -6,6 +6,7 @@ namespace locaweb_rest_api.Repositories
     {
         IEnumerable<SentEmail> GetAll(int page, int idUser);
         SentEmail? GetById(int id);
+        SentEmail? GetLast(int idUser);
         void Add(SentEmail model);
         void Update(SentEmail model);
         void Delete(SentEmail model);
