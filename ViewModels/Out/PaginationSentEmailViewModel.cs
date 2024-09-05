5c6873a9f2cd967b0b6e485638b57bb389e933be
Gustavo Lopes <guhrlopes999@gmail.com>
2024-09-05 13:29:53 -0300

    feat: Criar classe PaginationSentEmailViewModel no namespace ViewModels.Out

---
+++ ViewModels/Out/PaginationSentEmailViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace locaweb_rest_api.ViewModels.Out
+{
+    public class PaginationSentEmailViewModel
+    {
+        public required IEnumerable<OutSentEmailViewModel> SentEmails { get; set; }
+        public int CurrentPage { get; set; }
+        public readonly int PageSize = 20;
+        public bool HasPreviousPage => CurrentPage > 1;
+        public bool HasNextPage => SentEmails.Count() == PageSize;
+        public string PreviousPageUrl => HasPreviousPage ? $"/api/SentEmail?page={CurrentPage - 1}" : "";
+        public string NextPageUrl => HasNextPage ? $"/api/SentEmail?page={CurrentPage + 1}" : "";
+    }
+}
