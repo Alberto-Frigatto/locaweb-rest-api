7488982d41a86a58c1efa2fda7c77eba5e2639c5
Gustavo Lopes <guhrlopes999@gmail.com>
2024-09-05 13:27:40 -0300

    fix: Alterar atributos da classe InCreateSentEmailViewModel
    - Altera a validação do atributo Recipient
    - Remove o atributo IdUser
    - Adiciona o atributo SendDate

--- ViewModels/In/InCreateSentEmailViewModel.cs
+++ ViewModels/In/InCreateSentEmailViewModel.cs
@@ -5,8 +5,9 @@ namespace locaweb_rest_api.ViewModels.In
     public class InCreateSentEmailViewModel
     {
         [Required(ErrorMessage = "O destinatário é obrigatório")]
-        [MinLength(3, ErrorMessage = "O destinatário deve ter pelo menos 3 caracteres")]
+        [MinLength(10, ErrorMessage = "O destinatário deve ter pelo menos 10 caracteres")]
         [MaxLength(150, ErrorMessage = "O destinatário não deve exceder 150 caracteres")]
+        [EmailAddress(ErrorMessage = "O e-mail é inválido")]
         public string Recipient { get; set; }
 
         [Required(ErrorMessage = "O assunto é obrigatório")]
@@ -18,9 +19,9 @@ namespace locaweb_rest_api.ViewModels.In
         [MinLength(1, ErrorMessage = "O corpo do e-mail não pode ser vazio")]
         [MaxLength(255, ErrorMessage = "O corpo do e-mail não pode exceder 255 caracteres")]
         public string Body { get; set; }
-        
-        [Required(ErrorMessage = "O id do usuário é obrigatório")]
-        [Range(1, int.MaxValue, ErrorMessage = "O id do usuário é inválido")]
-        public int IdUser { get; set; }
+
+        [Required(ErrorMessage = "O dia de envio é obrigatório")]
+        [RegularExpression(@"\d{2}/\d{2}/\d{4}", ErrorMessage = "A data deve estar no formato dd/MM/aaaa")]
+        public string SendDate { get; set; }
     }
 }
