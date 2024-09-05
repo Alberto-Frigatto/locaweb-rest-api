e678ed415e359c2a9fa3c9df9fc8710fbf477b44
Gustavo Lopes <guhrlopes999@gmail.com>
2024-09-05 13:29:34 -0300

    feat: Criar classe PaginationReceivedEmailViewModel no namespace ViewModels.Out

---
+++ ViewModels/Out/PaginationReceivedEmailViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace locaweb_rest_api.ViewModels.Out
+{
+    public class PaginationReceivedEmailViewModel
+    {
+        public required IEnumerable<OutReceivedEmailViewModel> ReceivedEmails { get; set; }
+        public int CurrentPage { get; set; }
+        public readonly int PageSize = 20;
+        public bool HasPreviousPage => CurrentPage > 1;
+        public bool HasNextPage => ReceivedEmails.Count() == PageSize;
+        public string PreviousPageUrl => HasPreviousPage ? $"/api/ReceivedEmail?page={CurrentPage - 1}" : "";
+        public string NextPageUrl => HasNextPage ? $"/api/ReceivedEmail?page={CurrentPage + 1}" : "";
+    }
+}
