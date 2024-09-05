848c7c57743c07df00b87efdc37e5795d02b374c
Gustavo Lopes <guhrlopes999@gmail.com>
2024-09-05 13:30:55 -0300

    feat: Adicionar método GetLastSentEmail à interface ISentEmailService

--- Services/ISentEmailService.cs
+++ Services/ISentEmailService.cs
@@ -6,6 +6,7 @@ namespace locaweb_rest_api.Services
     {
         IEnumerable<SentEmail> GetAllSentEmails(int page, int idUser);
         SentEmail? GetSentEmailById(int id);
+        SentEmail? GetLastSentEmail(int idUser);
         void CreateSentEmail(SentEmail model);
         void UpdateSentEmail(SentEmail model);
         void DeleteSentEmail(int id);
