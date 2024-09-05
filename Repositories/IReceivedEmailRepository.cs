3bd5e51c1c97ce5b6a922ceadf672e16fb5d907d
Gustavo Lopes <guhrlopes999@gmail.com>
2024-09-05 17:13:05 -0300

    feat: Adicionar método Search na interface IReceivedEmailRepository

--- Repositories/IReceivedEmailRepository.cs
+++ Repositories/IReceivedEmailRepository.cs
@@ -5,6 +5,7 @@ namespace locaweb_rest_api.Repositories
     public interface IReceivedEmailRepository
     {
         IEnumerable<ReceivedEmail> GetAll(int page, int idUser);
+        IEnumerable<ReceivedEmail> Search(string query, int page, int idUser);
         ReceivedEmail? GetById(int id, int idUser);
     }
 }
