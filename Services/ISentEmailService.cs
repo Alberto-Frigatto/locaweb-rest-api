1fa081125f1c5e2c8fe32fc18a7843415adee510
Gustavo Lopes <guhrlopes999@gmail.com>
2024-09-05 20:38:41 -0300

    refactor: Alterar classe ISentEmailService no namespace Services

--- Services/ISentEmailService.cs
+++ Services/ISentEmailService.cs
@@ -5,6 +5,7 @@ namespace locaweb_rest_api.Services
     public interface ISentEmailService
     {
         IEnumerable<SentEmail> GetAllSentEmails(int page, int idUser);
+        IEnumerable<SentEmail> SearchSentEmails(string query, int page, int idUser);
         SentEmail? GetSentEmailById(int id);
         SentEmail? GetLastSentEmail(int idUser);
         void CreateSentEmail(SentEmail model);
