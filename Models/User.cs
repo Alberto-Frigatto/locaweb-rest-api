e47abe5e153a304fea81579b0ef6a605d1e05e72
Gustavo Lopes <guhrlopes999@gmail.com>
2024-09-09 21:16:41 -0300

    refactor: Alterar classe User no namespace Models

--- Models/User.cs
+++ Models/User.cs
@@ -6,7 +6,6 @@
         public string Email { get; set; }
         public string FullName { get; set; }
         public string Password { get; set; }
-        public string Image {  get; set; }
         public string Language { get; set; }
         public bool Theme { get; set; }
     }
