794ace2eaf5ad0448a2fa1ba19e46f1bf6a017b5
Gustavo Lopes <guhrlopes999@gmail.com>
2024-09-09 21:21:10 -0300

    feat: Adicionar migrações

---
+++ Migrations/20240907201247_RemoveImageAttrFromUser.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace locaweb_rest_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveImageAttrFromUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "User");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "User",
+                type: "NVARCHAR2(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
