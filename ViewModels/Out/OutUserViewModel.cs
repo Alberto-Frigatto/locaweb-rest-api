047f6c300d6459e231e1df9593645bbe4b0c3273
Gustavo Lopes <guhrlopes999@gmail.com>
2024-09-09 21:17:11 -0300

    refactor: Alterar classe OutUserViewModel no namespace ViewModels

--- ViewModels/Out/OutUserViewModel.cs
+++ ViewModels/Out/OutUserViewModel.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public string Email { get; set; }
         public string FullName { get; set; }
-        public string Image { get; set; }
         public string Language { get; set; }
         public bool Theme { get; set; }
     }
