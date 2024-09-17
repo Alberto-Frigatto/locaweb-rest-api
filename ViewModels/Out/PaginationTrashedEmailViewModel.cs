8bf5d6115a45d833cf4107e7385e8ce62304faae
Gustavo Lopes <guhrlopes999@gmail.com>
2024-09-17 18:47:55 -0300

    feat: Criar classe PaginationTrashedEmailViewModel no namespace ViewModels.Out

---
+++ ViewModels/Out/PaginationTrashedEmailViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace locaweb_rest_api.ViewModels.Out
+{
+    public class PaginationTrashedEmailViewModel
+    {
+        public required IEnumerable<object> TrashedEmails { get; set; }
+        public int CurrentPage { get; set; }
+        public readonly int PageSize = 20;
+        public bool HasPreviousPage => CurrentPage > 1;
+        public bool HasNextPage => TrashedEmails.Count() == PageSize;
+        public string PreviousPageUrl => HasPreviousPage ? $"/api/TrashedEmail?page={CurrentPage - 1}" : "";
+        public string NextPageUrl => HasNextPage ? $"/api/TrashedEmail?page={CurrentPage + 1}" : "";
+    }
+}
